be6a5f493adc0f8ed6084bd3f1b0183b11820883
Rajshree <rajshreeakm@gmail.com>
2025-04-17 01:53:09 +0530

    Updated contact section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,9 +3,17 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
+import { motion } from "framer-motion";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
+    <>
+    <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>Tech Stack</p>
+        <h2 className={`${styles.sectionHeadText}`}>Skills.</h2>
+      </motion.div>
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
         <div className='w-28 h-28' key={technology.name}>
@@ -13,6 +21,7 @@ const Tech = () => {
         </div>
       ))}
     </div>
+    </>
   );
 };
 
